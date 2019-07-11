6ed194aa7e4403d27bff49510422ea6447e07992
fuqiang1996 <315458488@qq.com>
2019-07-11 08:53:01 +0800

    增加收入,完善程序

---
+++ miniprogram/pages/user-center/income.js
@@ -0,0 +1,46 @@
+const db = wx.cloud.database({});
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    label: '',
+    consume:[],
+    allMoney:0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  onShow: function (options) {
+    let that =this;
+    db.collection('consume').get().then(res => {
+      let allMoney = 0;
+      let consume = [];
+      for (let i=0; i< res.data.length; i++){
+        allMoney += Number.parseFloat(res.data[i].money);
+        consume.push(res.data[i]);
+      }
+      that.setData({
+        consume: consume,
+        allMoney: allMoney,
+      })
+    })
+  },
+  /**
+   * 更新或者删除
+   */
+  updateClick: function (e) {
+    let id = e.target.id;
+    wx.navigateTo({
+      url:"/pages/user-center/updateOrdelete?id="+id,
+    })
+  },
+
+
+})
