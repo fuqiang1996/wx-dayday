2e09b46870d45d6aecd0bf48cbc7116c536bf337
fuqiang1996 <315458488@qq.com>
2019-07-12 14:56:51 +0800

    修复bug

--- miniprogram/pages/user-center/cost-colunm.js
+++ miniprogram/pages/user-center/cost-colunm.js
@@ -2,14 +2,14 @@ import F2 from '../../f2-canvas/lib/f2';
 
 let chart = null;
 let data = [
-  { year: '1951 年', sales: 38 },
-  { year: '1952 年', sales: 52 },
-  { year: '1956 年', sales: 61 },
-  { year: '1957 年', sales: 145 },
-  { year: '1958 年', sales: 48 },
-  { year: '1959 年', sales: 38 },
-  { year: '1960 年', sales: 38 },
-  { year: '1962 年', sales: 38 },
+  { month: '1 月', sales: 4000 },
+  { month: '2 月', sales: 2034 },
+  { month: '3 月', sales: 61 },
+  { month: '4 月', sales: 2000 },
+  { month: '5 月', sales: 48 },
+  { month: '6 月', sales: 38 },
+  { month: '7 月', sales: 38 },
+  { month: '8 月', sales: 38 },
 ];
 function initChart(canvas, width, height) {
 
@@ -25,7 +25,7 @@ function initChart(canvas, width, height) {
     }
   });
   chart.tooltip(false);
-  chart.interval().position('year*sales');
+  chart.interval().position('month*sales');
   chart.render();
 
   // 绘制柱状图文本
@@ -46,7 +46,7 @@ function initChart(canvas, width, height) {
       }
     });
 
-    shapes[obj.year] = text; // 缓存该 shape, 便于后续查找
+    shapes[obj.month] = text; // 缓存该 shape, 便于后续查找
   });
 
   let lastTextShape; // 上一个被选中的 text
@@ -64,7 +64,7 @@ function initChart(canvas, width, height) {
         fontWeight: 'normal'
       });
       if (selected) {
-        const textShape = shapes[data.year];
+        const textShape = shapes[data.month];
         textShape.attr({
           fill: '#000',
           fontWeight: 'bold'
